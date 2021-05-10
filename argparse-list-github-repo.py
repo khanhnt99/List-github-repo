fdf5fbdee06face4c32a8359bc2b97b4b45c9e4a
khanhnt99 <ntkhanh99.vn@gmail.com>
2021-05-10 23:28:53 +0700

    doing argparse

---
+++ argparse-list-github-repo.py
@@ -0,0 +1,15 @@
+import requests
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='List Github repositories')
+    parser = add_argument('-t')
+    # res = requests.get('https://api.github.com/users/'+username+'/repos')
+    # json = res.json()
+    # for i in range(0, len(json)):
+    #     print("Project Number: ", i+1)
+    #     print("Project Name: ", json[i]['name'])
+
+
+if __name__ == '__main__':
+    main()
--- githubrepos.py
+++ githubrepos.py
@@ -1,7 +1,15 @@
 import requests
-username = input("Enter the github username:")
-res = requests.get('https://api.github.com/users/'+username+'/repos')
-json = res.json()
-for i in range(0, len(json)):
-    print("Project Number: ", i+1)
-    print("Project Name: ", json[i]['name'])
+import argparse
+
+
+def main(*args):
+    username = input("Enter the github username:")
+    res = requests.get('https://api.github.com/users/'+username+'/repos')
+    json = res.json()
+    for i in range(0, len(json)):
+        print("Project Number: ", i+1)
+        print("Project Name: ", json[i]['name'])
+    
+
+if __name__ == '__main__':
+        main()
