8e971f386f915c7c1fda73cf6db9dea2378a2fc8
khanhnt99 <ntkhanh99.vn@gmail.com>
2021-05-10 22:58:17 +0700

    done list github repo user

--- githubrepos.py
+++ githubrepos.py
@@ -1,16 +1,7 @@
 import requests
-import sys
-import json
-
-
-
-
-resp = requests.get("https://api.github.com/users/khanhnt99/repos")
-data = resp.json()
-
-print(data)
-
-
-
-
-
+username = input("Enter the github username:")
+res = requests.get('https://api.github.com/users/'+username+'/repos')
+json = res.json()
+for i in range(0, len(json)):
+    print("Project Number: ", i+1)
+    print("Project Name: ", json[i]['name'])
