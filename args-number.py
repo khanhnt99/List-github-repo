69acffd3cae78f16cbec506bec959479bcbd9d63
khanhnt <you@example.com>
2021-09-04 21:49:33 +0700

    update remove
    Signed-off-by: khanhnt <you@example.com>

--- args-number.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Process some interger.')
-parser.add_argument('intergers', metavar='N', type=int, nargs='+', 
-                    help='an interger for accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',const=sum, default=min, help='sum the interger')
-args = parser.parse_args()
-print(args.accumulate(args.intergers))
